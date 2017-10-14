c1716840ea4b8879a1afc423a5069fa78c3bd53d
BUIQUOCDAT1281997 <31703769+BUIQUOCDAT1281997@users.noreply.github.com>
2017-10-15 05:13:31 +0700

    Update Simple.kt

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -51,11 +51,7 @@ fun main(args: Array<String>) {
  * Пользователь задает время в часах, минутах и секундах, например, 8:20:35.
  * Рассчитать время в секундах, прошедшее с начала суток (30035 в данном случае).
  */
-fun seconds(hours: Int, minutes: Int, seconds: Int): Int {
-    val kq = hours * 60 * 60 + minutes * 60 + seconds
-    return kq
-}
-
+fun seconds(hours: Int, minutes: Int, seconds: Int): Int = hours * 60 * 60 + minutes * 60 + seconds
 /**
  * Тривиальная
  *
@@ -63,10 +59,7 @@ fun seconds(hours: Int, minutes: Int, seconds: Int): Int {
  * Определить длину того же отрезка в метрах (в данном случае 18.98).
  * 1 сажень = 3 аршина = 48 вершков, 1 вершок = 4.445 см.
  */
-fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double {
-    val tong = ((sagenes * 48 + arshins * 48/3 + vershoks) * 0.04445)
-    return tong
-}
+fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double  = ((sagenes * 48 + arshins * 48/3 + vershoks) * 0.04445)
 
 /**
  * Тривиальная
@@ -74,7 +67,7 @@ fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double {
  * Пользователь задает угол в градусах, минутах и секундах (например, 36 градусов 14 минут 35 секунд).
  * Вывести значение того же угла в радианах (например, 0.63256).
  */
-fun angleInRadian(grad: Int, min: Int, sec: Int): Double =( grad + (min/60) + sec/sqr(60.0))*(PI/180)
+fun angleInRadian(grad: Int, min: Int, sec: Int): Double =( grad + (min/60.0) + sec/sqr(60.0)*1.0)*(PI/180.0)
 
 /**
  * Тривиальная
@@ -86,7 +79,6 @@ fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double{
     val x = x2-x1
     val y = y2-y1
     val bien = sqrt( sqr(x)+sqr(y))
-
     return bien
 }
 
@@ -108,14 +100,7 @@ fun thirdDigit(number : Int): Int{
  * прибыл на станцию назначения в h2 часов m2 минут того же дня (например в 13:01).
  * Определите время поезда в пути в минутах (в данном случае 216).
  */
-fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int): Int{
-    val x1 = minutesArrive- minutesDepart
-    val x2 = hoursArrive- hoursDepart
-    var x3 =x2*60+x1
-
-    return x3
-}
-
+fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int): Int=(hoursArrive- hoursDepart)*60+(minutesArrive- minutesDepart)
 /**
  * Простая
  *
@@ -123,14 +108,7 @@ fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minute
  * Сколько денег будет на счету через 3 года (с учётом сложных процентов)?
  * Например, 100 рублей под 10% годовых превратятся в 133.1 рубля
  */
-fun accountInThreeYears(initial: Int, percent: Int): Double{
-//val x: Double= initial. toDouble()
-    val x: Double= percent.toDouble()
-    val sd= pow((1+ x/100), 3.0)
-    val kq =initial * sd
-    return kq
-}
-
+fun accountInThreeYears(initial: Int, percent: Int): Double=initial * pow((1+ percent.toDouble()/100), 3.0)
 /**
  * Простая
  *
